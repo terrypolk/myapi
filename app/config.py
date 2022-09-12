f6e9315f8a66ca25da690185da91325d1e01f6af
Terry Polk <polk@lilly.com>
2022-09-12 05:05:15 -0400

    cleanup

--- app/config.py
+++ app/config.py
@@ -2,16 +2,15 @@
 Common configuration settings
 """
 import os
-from pathlib import Path
 
 from dotenv import load_dotenv
 
 from app import globalvars  # Not used here, just getting a path from it.
 
-# Load variables from config file.
-load_dotenv(Path(os.environ["HOME"]) / ".env_app")
-DEBUG = os.environ["DEBUG"] == "true"
-
 # Evaluate dynamic values.
-APP_PATH = os.path.dirname(globalvars.__file__)
-GIT_PATH = APP_PATH + "/../.git"
+APP_PATH = os.path.dirname(globalvars.__file__) + "/../"
+GIT_PATH = APP_PATH + ".git"
+
+# Load variables from config file.
+load_dotenv(f"{APP_PATH}.env")
+DEBUG = os.environ["DEBUG"] == "true"
